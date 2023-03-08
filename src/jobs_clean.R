0f99af1571843c1dee1145191eab8f14f4f37f25
alz-droid <alzenpunio@gmail.com>
2023-03-08 16:25:36 +1300

    new .csv file and updated the cleaning program.

--- src/jobs_clean.R
+++ src/jobs_clean.R
@@ -4,4 +4,14 @@ library(lubridate)
 library(glue)
 library(furrr)
 
-analytics_jobs_df <- read_csv()
+analytics_jobs_df <- read_csv("https://raw.githubusercontent.com/alz-droid/jobs-web-scrape/main/data/analytics_jobs.csv")
+
+analytics_jobs_df <- analytics_jobs_df %>% 
+  mutate(Company = str_replace(Company, "at ", ""),
+         Location =  if_else(grepl("Manawatu", Location), "Manawatū", Location) %>% str_replace_all(".*, ", ""),
+         Description = str_replace_all(Description, "[\r\n\t]", " "))
+
+analytics_jobs_df <- analytics_jobs_df %>% 
+  mutate(Date_Posted = if_else(grepl("d ago", Date_Posted),
+                               as_date(format(Sys.Date() - days(parse_number(Date_Posted)), "%d-%m-%Y")),
+                               as_date(format(Sys.Date(), "%d-%m-%Y"))))
